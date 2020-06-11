a195f3c8909f7a858755594dff656a71605d059a
Nader <na511dr@hotmail.com>
2020-06-11 14:00:50 +0300

    Updates

---
+++ precache-manifest.ccbab4ba3d4e3eb2238a3251fdafdcde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ef99ab5e6c628039f195a74e76bf99",
+    "url": "/manegizer/index.html"
+  },
+  {
+    "revision": "3d084b31be49dce9ab27",
+    "url": "/manegizer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ab59146cd4521a5290bf",
+    "url": "/manegizer/static/css/main.3ed61038.chunk.css"
+  },
+  {
+    "revision": "3d084b31be49dce9ab27",
+    "url": "/manegizer/static/js/2.21282853.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/manegizer/static/js/2.21282853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab59146cd4521a5290bf",
+    "url": "/manegizer/static/js/main.1f7e1efe.chunk.js"
+  },
+  {
+    "revision": "1401d3af8bbc5da1bbfa",
+    "url": "/manegizer/static/js/runtime-main.3e19d0f7.js"
+  }
+]);
